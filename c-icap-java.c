99227b987fc6f4134337a0633ac783980c4d0e02
syakesaba <syakesaba@gmail.com>
2014-09-28 12:15:04 +0900

    未テスト

--- c-icap-java.c
+++ c-icap-java.c
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 #include "jni.h"
 
 #include "c_icap/c-icap.h"
@@ -22,14 +27,13 @@
 
 #define MAX_JVM_OPTIONS 4
 
-typedef struct jData {
-    JNIEnv jni;
+typedef struct jDataStruct {
+    JNIEnv * jni;
     JavaVM * jvm;
-    JavaVMOption * jvmOptions;
+    JavaVMOption jvmOptions[4];
     jclass jIcapClass;
     const char * name;
     const int mod_type;
-    jobject jIcapInstance;
     jmethodID jInitService;
     jmethodID jPostInitService;
     jmethodID jCloseService;
@@ -38,7 +42,14 @@ typedef struct jData {
     jmethodID jCheckPreviewHandler;
     jmethodID jEndOfDataHandler;
     jmethodID jServiceIO;
+    u_int32_t times_served;
 } jData_t;
+
+typedef struct jServiceDataStruct {
+    jData_t * jdata;
+    u_int32_t request_id;
+    jobject instance;
+} jServiceData_t;
 
 int init_java_handler(struct ci_server_conf * server_conf);
 int post_init_java_handler(struct ci_server_conf * server_conf);
@@ -66,14 +77,15 @@ CI_DECLARE_DATA service_handler_module_t module = {
 
 ci_dyn_array_t * enviroments;
 #define MAX_ENVIRONMENTS_SIZE 256
+#define CIJ_CLASS_MOD_TYPE "MOD_TYPE"
 
-const char * LIBRARIES_PATH;
+
+const char * JAVA_CLASS_PATH;
+const char * JAVA_LIBRARY_PATH;
 
 int init_java_handler(struct ci_server_conf * server_conf) {
-    enviroments = ci_dyn_array_new(MAX_ENVIRONMENTS_SIZE);
-    cij_debug_printf(CIJ_INFO_LEVEL, "SERVICES_DIR: %s", server_conf->SERVICES_DIR);
-    cij_debug_printf(CIJ_INFO_LEVEL, "MODULES_DIR: %s", server_conf->MODULES_DIR);
-    LIBRARIES_PATH = server_conf->SERVICES_DIR;//:server_conf->MODULES_DIR;
+    enviroments = ci_dyn_array_new(MAX_ENVIRONMENTS_SIZE);//FREEME
+    JAVA_CLASS_PATH = server_conf->SERVICES_DIR;
     return CI_OK;
 }
 
@@ -84,33 +96,83 @@ int post_init_java_handler(struct ci_server_conf * server_conf) {
 ci_service_module_t * load_java_module(char * service_file) {
     ci_service_module_t * service = NULL;
     jData_t * jdata = NULL;
-    service = malloc(sizeof(ci_service_module_t));
-    jdata = malloc(sizeof(jData_t));
+    service = malloc(sizeof(ci_service_module_t));//FREEME
+    jdata = malloc(sizeof(jData_t));//FREEME
     if (service == NULL || jdata == NULL) {
         cij_debug_printf(CIJ_ERROR_LEVEL,"Failed to allocate memory for service %s",service_file);
         return NULL;
     }
+    jdata->times_served = 0;
+
     //set service file name to SERVICE NAME
     const char * service_file_name = basename(service_file);
-    const char * name = strdup(service_file_name);
+    char * name = strndup(service_file_name, MAX_SERVICE_NAME);//FREEME
+    name[strnlen(name, MAX_SERVICE_NAME)-strnlen(".class", MAX_SERVICE_NAME)] = '\0';//strip ".class"
     jdata->name = name;
 
+    {
     //set CLASSPATH
     JavaVMOption * options = jdata->jvmOptions;
-    options[0].optionString = LIBRARIES_PATH;
+    sprintf(options[0].optionString, "-Djava.class.path=%s",JAVA_CLASS_PATH);
+    //options[1].optionString = "-verbose:jni";
+    //options[2].optionString = sprintf("-Djava.library.path=%s",LIBRARIES_PATH);
+    //options[3].optionString = "";
 
+    //create VM
     JavaVMInitArgs jvmInitArgs;
     jvmInitArgs.options = options;
-    jvmInitArgs.nOptions = 2; //TODO:CLASSPATH,VERSION,...
-    jvmInitArgs.version = JNI_VERSION_1_8;
-    jvmInitArgs.ignoreUnrecognized = JNI_VERSION_1_8;
+    jvmInitArgs.nOptions = 1; //TODO:CLASSPATH,VERSION,...
+    jvmInitArgs.version = JNI_VERSION_1_6;
+    jvmInitArgs.ignoreUnrecognized = JNI_FALSE;
+    jint ret = JNI_CreateJavaVM(&(jdata->jvm), (void **)&(jdata->jni), &jvmInitArgs);
+    if (ret != JNI_OK) {
+        cij_debug_printf(CIJ_ERROR_LEVEL, "Failed to setup JavaVM(%d).", ret);
+        goto FAIL_TO_LOAD_SERVICE;
+    }
+    }
 
-    JNI_CreateJavaVM(&(jdata->jvm), (void **)&(jdata->jni), &jvmInitArgs);
-    //initialize jvm;
-    //load class
-    //identify REQMOD, RESPMOD
+    JNIEnv * jni = jdata->jni;
 
-    service->mod_data = NULL;
+    //find class
+    {
+    const char * className = NULL;
+    snprintf(className, "L%s;", name);
+    jclass service_class = (*jni)->FindClass(jni, className);
+    if (service_class == NULL) {
+        cij_debug_printf(CIJ_ERROR_LEVEL, "Failed to find java class '%s'.", className);
+        goto FAIL_TO_LOAD_SERVICE;
+    }
+    jdata->jIcapClass = service_class;
+    }
+    jclass cls = jdata->jIcapClass;
+
+    //identify REQMOD, RESPMOD 'int class.MOD_TYPE'
+    jfieldID mod_type_fid = (*jni)->GetStaticFieldID(jni, cls, "si", CIJ_CLASS_MOD_TYPE);
+    if (mod_type_fid == NULL) {
+        cij_debug_printf(CIJ_ERROR_LEVEL, "Failed to find field '%s'.", CIJ_CLASS_MOD_TYPE);
+        goto FAIL_TO_LOAD_SERVICE;
+    }
+    jint mod_type = jni->GetStaticIntField(&jni, cls, mod_type_fid);
+    jdata->mod_type = mod_type;
+
+    //TODO: get <init> for init_request_data
+
+    //TODO: get   int preview(String preview){} for check_preview_handler
+
+    //TODO: get   byte[] service(byte[] data){} for end_of_data_handler
+
+    //get init_service()
+    jfieldID mod_type_fid = (*jni)->GetStaticFieldID(jni, cls, "si", CIJ_CLASS_MOD_TYPE);
+    if (mod_type_fid == NULL) {
+        cij_debug_printf(CIJ_ERROR_LEVEL, "Failed to find field '%s'.", CIJ_CLASS_MOD_TYPE);
+        goto FAIL_TO_LOAD_SERVICE;
+    }
+    jint mod_type = jni->GetStaticIntField(&jni, cls, mod_type_fid);
+    jdata->mod_type = mod_type;
+
+    //TODO: stdout,stdin => c-icap's std
+
+    service->mod_data = (void *)jdata;
     service->mod_conf_table = NULL;
     service->mod_init_service = java_init_service;
     service->mod_post_init_service = java_post_init_service;
@@ -123,10 +185,31 @@ ci_service_module_t * load_java_module(char * service_file) {
     service->mod_name = name;
     service->mod_type = ICAP_REQMOD | ICAP_RESPMOD;
     ci_debug_printf(1, "OK service %s loaded\n", service_file);
+    ci_dyn_array_add(enviroments, name, jdata, sizeof(jData_t *));
     return service;
+
+FAIL_TO_LOAD_SERVICE:
+    free(name);
+    free(service);
+    free(jdata);
+    cij_debug_printf(CIJ_ERROR_LEVEL, "Fail at loading service '%s'",service_file);
+    ci_dyn_array_destroy(enviroments);
+    return NULL;
+}
+
+int killVM(void *data, const char *name, const void * value) {
+    const jData_t * jdata = (jData_t *)value;
+    const char * icapName = jdata->name;
+    free((void *)icapName);
+    JavaVM * jvm = (jdata->jvm);
+    jint ret = (*jvm)->DestroyJavaVM(jvm);
+    free((void *)jdata);
+    return ret;
 }
 
 void release_java_handler() {
+    //iterate and kill all JavaVMs
+    ci_dyn_array_iterate(enviroments, NULL, killVM);
     return;
 }
 
@@ -142,22 +225,39 @@ int java_post_init_service(ci_service_xdata_t * srv_xdata, struct ci_server_conf
 }
 
 void java_close_service() {
+    //nop
 }
 
 void * java_init_request_data(ci_request_t * req) {
-    ci_headers_list_t * hdrs = ci_http_request_headers(req);
+    const int REQ_TYPE = ci_req_type(req);
+    const char * METHOD_TYPE = ci_method_string(REQ_TYPE);//Don't free!
+
+    ci_headers_list_t *hdrs = NULL;
+
+    if (REQ_TYPE == ICAP_REQMOD) {
+        hdrs = ci_http_request_headers(req);
+    } else if (REQ_TYPE == ICAP_RESPMOD) {
+        hdrs = ci_http_response_headers(req);
+    } else if (REQ_TYPE == ICAP_OPTIONS){
+        // something? ;
+        return NULL;
+    } else {
+        // UNKNOWN protocol
+        return NULL;
+    }
     //identify reqmod or respmod or else
-    //create new instance;
+    //create new instance calling constructor;
+    //XXX: init(int mod_type, String[] headers)
     return (void *) NULL;
 }
 
 void java_release_request_data(void * data) {
-    jData_t * jdata = (jData_t *) data;
-    free(jdata);
+    jServiceData_t * jServiceData = (jServiceData_t *)data;
+    JNIEnv * jni = (jServiceData->jdata)->jni;
+    (*jni)->DeleteLocalRef(jni, jServiceData->instance);
 }
 
 int java_check_preview_handler(char * preview_data, int preview_data_len, ci_request_t * req) {
-    jData_t * jdata = (jData_t *)ci_service_data(req);
     //check preview data
     //hasbody
     //lock or  unlock
@@ -166,13 +266,11 @@ int java_check_preview_handler(char * preview_data, int preview_data_len, ci_req
 }
 
 int java_end_of_data_handler(ci_request_t * req) {
-    jData_t * jdata = (jData_t *)ci_service_data(req);
     //replace headers
     return CI_MOD_DONE;
 }
 
 int java_service_io(char * wbuf, int * wlen, char * rbuf, int * rlen, int iseof, ci_request_t * req) {
-    jData_t * jdata = (jData_t *)ci_service_data(req);
     int ret;
 
     if (rlen && rbuf) {
